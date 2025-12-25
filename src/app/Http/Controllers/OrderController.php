ed0b838dbff0ad37eb8b3d8323986a086c995537
sin <lupkalcode1224@gmail.com>
2025-12-25 14:34:28 +0900

    Add order creation API

--- src/app/Http/Controllers/OrderController.php
+++ src/app/Http/Controllers/OrderController.php
@@ -2,9 +2,53 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Menu;
+use App\Models\Order;
+use App\Models\OrderItem;
+use Illuminate\Support\Facades\Log;
+
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class OrderController extends Controller
 {
-    
+    public function store(Request $request) {
+        $validated = $request->validate([
+            'tableNumber' => 'required|integer',
+            'items' => 'required|array|min: 1',
+            'items.*.menuId' => 'required|integer|exists:menus,id',
+            'items.*.quantity' => 'required|integer|min: 1',   
+        ]);
+
+        try{
+        DB::transaction(function () use ($validated){
+            $order = Order::create([
+                'table_number' => $validated['tableNumber'],
+            ]);
+
+            $total = 0;
+
+            foreach($validated['items'] as $item) {
+                $menu = Menu::findOrFail($item['menuId']);
+                OrderItem::create([
+                    'menu_id' => $menu->id,
+                    'order_id' => $order->id,
+                    'price_at_order' => $menu->price,
+                    'quantity' => $item['quantity'],
+                ]);
+                 $total += $menu->price * $item['quantity'];
+            }
+                $order->update(['total_price' => $total]);
+            });
+
+            return response()->json([
+                    'message' => '注文が完了しました'
+                ], 201);
+        }catch(\Throwable $e) {
+             Log::error($e);
+             return response()->json([
+            'message' => '注文の作成に失敗しました'
+        ], 500);
+        }
+    }   
 }
--- src/routes/api.php
+++ src/routes/api.php
@@ -2,5 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MenuController;
+use App\Http\Controllers\OrderController;
 
 Route::get('/menus', [MenuController::class, 'index']);
+Route::post('/orders', [OrderController::class, 'store']);
