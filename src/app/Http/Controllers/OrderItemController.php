cbd125992d1e2ec28489c9f6f477b39df0daa8c9
sin <lupkalcode1224@gmail.com>
2026-01-08 22:47:19 +0900

    refactor: simplify order item index controller

--- src/app/Http/Controllers/OrderItemController.php
+++ src/app/Http/Controllers/OrderItemController.php
@@ -2,24 +2,14 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Models\Order;
-use Illuminate\Support\Facades\Log;
 
 class OrderItemController extends Controller
 {
     public function index(Order $order) {
-        try{
             $items = $order->items()->with('menu')->get();
             return response()->json([
                 'data' => $items
             ], 200);
-        }catch(\Throwable $e){
-            Log::error($e);
-
-            return response()->json([
-                'message' => 'サーバーエラーです'
-            ], 500);
-        }
     }
 }
